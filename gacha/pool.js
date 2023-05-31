cbe5857f85f9763159a1ea576378cda2a1264a8a
xswzaq123 <129960546+xswzaq123@users.noreply.github.com>
2023-05-31 22:28:26 +0530

    Add files via upload

---
+++ gacha/pool.js
@@ -0,0 +1,352 @@
+export const pool = {
+    "fivestar": {
+        baseChance: 0.006,
+        consolidatedChance: 0.016,
+        "items": {
+            baseChance: 0.5,
+            "featured": {
+                        itemName: "Jing Yuan",
+                        rarity: 5,
+                        pic: "./assets/jing.png",
+            },
+            "nonFeatured": {
+                "1": {
+                        itemName: "Bronya",
+                        rarity: 5,
+                        pic: "./assets/bronya.png",
+                },
+                "2": {
+                        itemName: "Clara",
+                        rarity: 5,
+                        pic: "./assets/clara.png",
+                },
+                "3": {
+                        itemName: "Himeko",
+                        rarity: 5,
+                        pic: "./assets/himeko.png",
+                },
+                "4": {
+                        itemName: "Welt",
+                        rarity: 5,
+                        pic: "./assets/welt.png",
+                },
+                "5": {
+                        itemName: "Gepard",
+                        rarity: 5,
+                        pic: "./assets/gepard.png",
+                },
+                "6": {
+                        itemName: "Bailu",
+                        rarity: 5,
+                        pic: "./assets/bailu.png",
+                },
+                "7": {
+                        itemName: "Yanqing",
+                        rarity: 5,
+                        pic: "./assets/yanqing.png",
+                },
+            },
+        },
+    },
+    "fourstar": {
+        baseChance: 0.051,
+        consolidatedChance: 0.13,
+        "characters": {
+            baseChance: 0.0255,
+            "items": {
+                baseChance: 0.5,
+                "featured": {
+                    "1": {
+                        itemName: "Sushang",
+                        rarity: 4,
+                        pic: "./assets/1.png",
+                    },
+                    "2": {
+                        itemName: "Tingyun",
+                        rarity: 4,
+                        pic: "./assets/2.png",
+                    },
+                    "3": {
+                        itemName: "March 7th",
+                        rarity: 4,
+                        pic: "./assets/3.png",
+                    },
+                },
+                "nonFeatured": {
+                    "1": {
+                        itemName: "Asta",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "2": {
+                        itemName: "Herta",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "3": {
+                        itemName: "Natasha",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "4": {
+                        itemName: "Arlan",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "5": {
+                        itemName: "Pela",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "6": {
+                        itemName: "Sampo",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "7": {
+                        itemName: "Hook",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "8": {
+                        itemName: "Dan Heng",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "9": {
+                        itemName: "Qingque",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                    "10": {
+                        itemName: "Serval",
+                        rarity: 4,
+                        pic: "./assets/lc.png",
+                    },
+                },
+            },
+        },
+        "lightCones": {
+            baseChance: 0.0255,
+            "items": {
+                "1": {
+                    itemName: "Post-Op Conversation",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "2": {
+                    itemName:  "Good Night and Sleep Well",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "3": {
+                    itemName:  "Day One of My New Life",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "4": {
+                    itemName:  "Only Silence Remains",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "5": {
+                    itemName:  "Memories of the Past",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "6": {
+                    itemName:  "The Moles Welcome You",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "7": {
+                    itemName:  "The Birth of Self",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "8": {
+                    itemName:  "Shared Feeling",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "9": {
+                    itemName:  "Eyes of the Prey",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "10": {
+                    itemName:  "Landau's Choice",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "11": {
+                    itemName:  "Swordplay",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "12": {
+                    itemName:  "Planetary Rendezvous",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "1lc": {
+                    itemName:  "A Secret Vow",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "14": {
+                    itemName:  "Make the World Clamor",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "15": {
+                    itemName:  "Perfect Timing",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "16": {
+                    itemName:  "Resolution Shines as Pearls of Sweat",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "17": {
+                    itemName:  "Trend of the Universal Market",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "18": {
+                    itemName:  "Subscribe for More!",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "19": {
+                    itemName:  "Dance!Dance!Dance!",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "20": {
+                    itemName:  "Under the Blue Sky",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+                "21": {
+                    itemName:  "Geniuses' Response",
+                    rarity: 4,
+                    pic: "./assets/lc.png",
+                },
+            },
+        },
+    },
+    "threeStar": {
+        baseChance: 0.943,
+        consolidatedChance: 0.854,
+        "items": {
+
+            "1": {
+                itemName:  "Arrows",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "2": {
+                itemName:  "Cornucopia",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "3": {
+                itemName:  "Collapsing Sky",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "4": {
+                itemName:  "Amber",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "5": {
+                itemName:  "Void",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "6": {
+                itemName:  "Chorus",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "7": {
+                itemName:  "Data Bank",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "8": {
+                itemName:  "Darting Arrow",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "9": {
+                itemName:  "Fine Fruit",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "10": {
+                itemName:  "Shattered Home",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "11": {
+                itemName:  "Defense",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "12": {
+                itemName:  "Loop",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "13": {
+                itemName:  "Meshing Cogs",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "14": {
+                itemName:  "Passkey",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "15": {
+                itemName:  "Adversarial",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "16": {
+                itemName:  "Multiplication",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "17": {
+                itemName:  "Mutual Demise",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "18": {
+                itemName:  "Pioneering",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "19": {
+                itemName:  "Hidden Shadow",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "20": {
+                itemName:  "Mediation",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+            "21": {
+                itemName:  "Sagacity",
+                rarity: 3,
+                pic: "./assets/lc.png",
+            },
+        },
+    },
+};
